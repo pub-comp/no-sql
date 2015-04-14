c5dc86c765b20f6dee6270f5089fd5267d1f516c
dannyvarod_cp <dd5a0369e14211a385f128a25405b173a470e69f7n6KD4hn>
2015-04-14 12:03:59 +0000

    v2.5.0
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS27]$/pubcomp/NoSql;C31004

--- NoSql.MongoDbDriver/MongoDbConnectionInfo.cs
+++ NoSql.MongoDbDriver/MongoDbConnectionInfo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace PubComp.NoSql.MongoDbDriver
@@ -7,12 +6,71 @@ namespace PubComp.NoSql.MongoDbDriver
     public class MongoDbConnectionInfo
     {
         private const string Prefix = @"mongodb://";
-        public string Host { get; set; }
-        public int Port { get; set; }
+        private const string DefaultHost = @"localhost";
+        private const int DefaultPort = 27017;
+        private const string DefaultDb = @"admin";
+
+        public IList<Instance> Instances { get; set; }
+
+        public string Host
+        {
+            get
+            {
+                if (Instances == null || !Instances.Any())
+                    return DefaultHost;
+
+                return Instances[0].Host;
+            }
+            set
+            {
+                var instances = Instances;
+
+                if (instances == null)
+                    instances = new List<Instance>();
+
+                var instance = instances.FirstOrDefault() ?? new Instance { Port = DefaultPort };
+                instance.Host = value;
+                if (!instances.Any())
+                    instances.Add(instance);
+
+                Instances = instances;
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                if (Instances == null || !Instances.Any())
+                    return DefaultPort;
+
+                return Instances[0].Port;
+            }
+            set
+            {
+                var instances = Instances;
+
+                if (instances == null)
+                    instances = new List<Instance>();
+
+                var instance = instances.FirstOrDefault() ?? new Instance { Host = DefaultHost };
+                instance.Port = value;
+                if (!instances.Any())
+                    instances.Add(instance);
+
+                Instances = instances;
+            }
+        }
+
         public string Username { get; set; }
+
         public string Password { get; set; }
+
+        public string LoginDb { get; set; }
+
         public string Db { get; set; }
-        public IList<KeyValuePair<string, string>> Options { get; private set; }
+
+        public IList<KeyValuePair<string, string>> Options { get; set; }
 
         public string ConnectionString
         {
@@ -29,29 +87,54 @@ namespace PubComp.NoSql.MongoDbDriver
                 }
 
                 string dbParams = string.Empty;
-                if (!string.IsNullOrEmpty(Db) || Options.Any())
+                if (!string.IsNullOrEmpty(LoginDb) || Options.Any())
                 {
-                    dbParams = '/' + (Db ?? string.Empty);
-                    if (Options.Any())
+                    dbParams = '/' + (LoginDb ?? string.Empty);
+                    if (Options != null && Options.Any())
                     {
                         dbParams += '?';
                         dbParams += string.Join(";", Options.Select(opt => opt.Key + '=' + opt.Value));
                     }
                 }
 
-                var result = string.Concat(Prefix, userPass, Host, ':', Port, dbParams);
+                var instances = string.Join(",", this.Instances);
+
+                var result = string.Concat(Prefix, userPass, instances, dbParams);
                 return result;
             }
         }
 
         public MongoDbConnectionInfo()
         {
-            this.Host = @"localhost";
-            this.Port = 27017;
+            this.Instances = new List<Instance> { new Instance { Host = DefaultHost, Port = DefaultPort } };
             this.Username = null;
             this.Password = null;
-            this.Db = "admin";
+            this.LoginDb = DefaultDb;
+            this.Db = DefaultDb;
             this.Options = new List<KeyValuePair<string, string>>();
+        }
+
+        public class Instance
+        {
+            public string Host { get; set; }
+            public int Port { get; set; }
+
+            public Instance()
+            {
+                this.Host = DefaultHost;
+                this.Port = DefaultPort;
+            }
+
+            public Instance(string host, int port)
+            {
+                this.Host = host;
+                this.Port = port;
+            }
+
+            public override string ToString()
+            {
+                return string.Concat(this.Host, ':', this.Port);
+            }
         }
     }
 }
